42d1fbe73803f1fe2c981db11cde46cba69eb342
vansh.shah <vanshshah174@gmail.com>
2025-06-21 12:13:00 -0400

    Add delete endpoint for favorites and improve code formatting in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,23 +2,24 @@ import express from "express";
 import { ENV } from "./config/env.js";
 import { db } from "./config/db.js";
 import { favoritesTable } from "./db/schema.js";
+import { and, eq} from "drizzle-orm";
 
 const app = express();
 const PORT = ENV.PORT || 5001;
 
 app.use(express.json());
 
-app.get("/api/health",(req,res) => {
-    res.status(200).json({success: true})
+app.get("/api/health", (req, res) => {
+    res.status(200).json({ success: true })
 })
 
-app.post("/api/favorites",async(req,res) => {
+app.post("/api/favorites", async (req, res) => {
 
     try {
-        const {userId,recipeId,title,image,cookTime,servings} = req.body;
+        const { userId, recipeId, title, image, cookTime, servings } = req.body;
 
-        if(!userId || !recipeId || !title){
-            return res.status(400).json({error: "Missing required fields"})
+        if (!userId || !recipeId || !title) {
+            return res.status(400).json({ error: "Missing required fields" })
         }
 
         const newFavorite = await db.insert(favoritesTable).values({
@@ -32,11 +33,27 @@ app.post("/api/favorites",async(req,res) => {
 
         res.status(201).json(newFavorite[0])
     } catch (error) {
-        console.log("Error adding favorite",error);
-        res.status(500).json({error: "Internal server error"})
+        console.log("Error adding favorite", error);
+        res.status(500).json({ error: "Internal server error" })
     }
 })
 
-app.listen(PORT,() => {
+app.delete("/api/favorites/:userId/:recipeId", async (req, res) => {
+    try {
+        const { userId, recipeId } = req.params;
+
+         await db.delete(favoritesTable).where(
+            and(eq(favoritesTable.userId, userId), eq(favoritesTable.recipeId, parseInt(recipeId)))
+        )
+
+        res.status(200).json({ message: "Favorites removed Successfully" })
+    } catch (error) {
+        console.log("Error removing a favorite",error)
+        res.status(500).json({ error: "Internal server error" })
+    }
+})
+
+
+app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
 })
